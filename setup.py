2b509ed3c8e059821b10fff86f24148a6d58a4c1
mdione <>
2005-09-24 00:53:20 +0000

    AM   trunk/setup.py A    trunk/MANIFEST.in * setup! dist!

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='psync',
+      version='0.2',
+      description="mirror sync'er",
+      long_description="""
+Psync aims to be a repository mirrorer with this goals:
+  * keep the mirror usable while it's updating.
+  * be able to handle any kind of repository.
+""",
+      author='Marcos D. Dione',
+      author_email='mdione@grulic.org.ar',
+      url='http://plantalta.homalinux.net/~mdione/projects/psync/',
+      license='GPL',
+      packages=['psync',
+                'psync.drivers',
+                ],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Natural Language :: Spanish',
+                   'Natural Language :: English',
+                   'Operating System :: POSIX',
+                   'Programming Language :: Python',
+                   'Topic :: Internet',
+                   ],
+      )
