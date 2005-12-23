8705e92ca4a6a2a0e7b80cad58c69e644a825d13
mdione <>
2005-12-23 20:13:40 +0000

    M    psync.conf.py * new config format. didn't finish converting everything, tho...

--- psync.conf.py
+++ psync.conf.py
@@ -5,149 +5,177 @@
 # example config file
 
 config= []
-config+= [dict(
-    local= 'test',
+#config+= [{
+    #'local': 'debian/updates/sarge',
+    #'driver': 'Debian',
+    #'url': 'http://security.debian.org/debian-security',
+    #'release': 'sarge/updates',
+    #'arch': 'i386',
+    #'modules': [ 'main', 'non-free', 'contrib' ],
+#}]
+#config+=[{
+    #'local': 'debian-non-US',
+    #'driver': 'Debian',
+    #'url': 'http://non-us.debian.org/debian-non-US',
+    #'release': 'sid/non-US',
+    #'arch': 'i386',
+    #'modules': [ 'main', 'non-free', 'contrib' ],
+#}]
+
+## deb ftp://ftp.nerim.net/debian-marillat/ unstable main
+#config+= [{
+    #'local': 'marillat',
+    #'driver': 'Debian',
+    #'url': 'ftp://ftp.nerim.net/debian-marillat',
+    #'release': 'sid',
+    #'arch': 'i386',
+    #'modules': [ 'main' ],
+#}]
+
+#config+= [{
+    #'local': 'plf-free',
+    #'driver': 'Urpmi',
+    #'url': 'http://ftp.club-internet.fr/pub/linux/plf/mandrake/free',
+    #'release': '10.2',
+    #'distroSpec': '%(release)s',
+    #'rpmSpec': '%(arch)s',
+    #'arch': 'i586',
+    #'modules': [''],
+    #'hdlist': '../synthesis.hdlist.cz',
+#}]
+
+#config+= [{
+    #'local': 'mandrake',
+    #'driver': 'Urpmi',
+    #'url': 'http://mirrors.kernel.org/mandrake/Mandrakelinux/official',
+    #'release': '2005',
+    #'arch': 'i586',
+    #'distroSpec': '%(release)s/%(arch)s',
+    #'rpmSpec': 'media/%(module)s',
+    #'modules': [ 'main', 'contrib', 'jpackage' ],
+    #'hdlist': '../media_info/synthesis.hdlist_%(module)s.cz',
+#}]
+
+#config+= [{
+    #'local': 'mandrake/updates',
+    #'driver': 'Urpmi',
+    #'url': 'http://mirrors.kernel.org/mandrake/Mandrakelinux/official/updates',
+    #'release': 'LE2005',
+    #'arch': 'i586',
+    #'distroSpec': '%(release)s',
+    #'rpmSpec': 'main_updates',
+    #'modules': [ '' ],
+    #'hdlist': 'media_info/synthesis.hdlist.cz',
+#}]
+
+## ubuntu
+config.append (dict (
+    repo= 'ubuntu',
+    repoUrl= 'http://archive.ubuntu.com/ubuntu',
+    repoDir= 'ubuntu',
     driver= 'Debian',
-    url= 'http://localhost:8099/~mdione/repo',
-    version= 'hoary-security',
-    arch= 'i386',
-    modules= [ 'main' ],
-)]
-# config+= [{
-#     'local': 'test',
-#     'driver': 'Debian',
-#     'url': 'http://localhost:8099/~mdione/repo',
-#     'distro': 'hoary-security',
-#     'arch': 'i386',
-#     'modules': [ 'main' ],
-# }]
+    releases= [ 'breezy' ],
+    archs= [ 'i386' ],
+    modules= [ 'main', 'restricted', 'universe', 'multiverse' ],
+    baseDirTemplate= '',
+))
 
-config+= [{
-    'local': 'debian/sarge',
-    'driver': 'Debian',
-    'url': 'http://http.us.debian.org/debian',
-    'version': 'sarge',
-    'arch': 'i386',
-    'modules': [ 'main', 'non-free', 'contrib', 'main/debian-installer' ],
-}]
-config+= [{
-    'local': 'debian/updates/sarge',
-    'driver': 'Debian',
-    'url': 'http://security.debian.org/debian-security',
-    'version': 'sarge/updates',
-    'arch': 'i386',
-    'modules': [ 'main', 'non-free', 'contrib' ],
-}]
-config+=[{
-    'local': 'debian-non-US',
-    'driver': 'Debian',
-    'url': 'http://non-us.debian.org/debian-non-US',
-    'version': 'sid/non-US',
-    'arch': 'i386',
-    'modules': [ 'main', 'non-free', 'contrib' ],
-}]
+#config+= [{
+    #'local': 'ubuntu/updates/hoary',
+    #'driver': 'Debian',
+    #'url': 'http://security.ubuntu.com/ubuntu',
+    #'release': 'hoary-security',
+    #'arch': 'i386',
+    #'modules': [ 'main' ],
+#}]
+config.append (dict (
+    repo= 'ubuntu-updates',
+    repoUrl= 'http://security.ubuntu.com/ubuntu',
+    repoDir= 'ubuntu/updates',
+    driver= 'Debian',
+    releases= [ 'breezy-security' ],
+    archs= [ 'i386' ],
+    modules= [ 'main', 'restricted' ],
+    baseDirTemplate= '',
+))
 
-# deb ftp://ftp.nerim.net/debian-marillat/ unstable main
-config+= [{
-    'local': 'marillat',
-    'driver': 'Debian',
-    'url': 'ftp://ftp.nerim.net/debian-marillat',
-    'version': 'sid',
-    'arch': 'i386',
-    'modules': [ 'main' ],
-}]
 
-config+= [{
-    'local': 'plf-free',
-    'driver': 'Urpmi',
-    'url': 'http://ftp.club-internet.fr/pub/linux/plf/mandrake/free',
-    'version': '10.2',
-    'distroSpec': '%(version)s',
-    'rpmSpec': '%(arch)s',
-    'arch': 'i586',
-    'modules': [''],
-    'hdlist': '../synthesis.hdlist.cz',
-}]
+#config+= [{
+    #'local': 'ubuntu-warty',
+    #'driver': 'Debian',
+    #'url': 'http://archive.ubuntu.com/ubuntu',
+    #'release': 'warty',
+    #'arch': 'i386',
+    #'modules': [ 'main', 'restricted', 'universe' ],
+#}]
 
-config+= [{
-    'local': 'mandrake',
-    'driver': 'Urpmi',
-    'url': 'http://mirrors.kernel.org/mandrake/Mandrakelinux/official',
-    'version': '2005',
-    'arch': 'i586',
-    'distroSpec': '%(version)s/%(arch)s',
-    'rpmSpec': 'media/%(module)s',
-    'modules': [ 'main', 'contrib', 'jpackage' ],
-    'hdlist': '../media_info/synthesis.hdlist_%(module)s.cz',
-}]
+#config+= [{
+    #'local': 'ubuntu/updates/breezy',
+    #'driver': 'Debian',
+    #'url': 'http://security.ubuntu.com/ubuntu',
+    #'release': 'breezy-security',
+    #'arch': 'i386',
+    #'modules': [ 'main' ],
+#}]
 
-config+= [{
-    'local': 'mandrake/updates',
-    'driver': 'Urpmi',
-    'url': 'http://mirrors.kernel.org/mandrake/Mandrakelinux/official/updates',
-    'version': 'LE2005',
-    'arch': 'i586',
-    'distroSpec': '%(version)s',
-    'rpmSpec': 'main_updates',
-    'modules': [ '' ],
-    'hdlist': 'media_info/synthesis.hdlist.cz',
-}]
+#config+= [{
+    #'local': 'fedora',
+    #'driver': 'Yum',
+    #'url': 'http://www.las.ic.unicamp.br/pub/fedora/linux/core',
+    #'release': '4',
+    #'arch': 'i386',
+    #'modules': [ '' ],
+    #'baseDir': "%(release)s/%(arch)s/os",
+    #'rpmDir': 'Fedora/RPMS',
+#}]
 
-# ubuntu
-config+= [{
-    'local': 'ubuntu-hoary',
-    'driver': 'Debian',
-    'url': 'http://archive.ubuntu.com/ubuntu',
-    'version': 'hoary',
-    'arch': 'i386',
-    'modules': [ 'main' ],
-}]
+#config+= [{
+    #'local': 'fedora/updates',
+    #'driver': 'Yum',
+    #'url': 'http://www.las.ic.unicamp.br/pub/fedora/linux/core/updates',
+    #'release': '3',
+    #'arch': 'i386',
+    #'modules': [ '' ],
+    #'baseDir': "%(release)s/%(arch)s",
+    #'rpmDir': '.',
+#}]
 
-config+= [{
-    'local': 'ubuntu/updates/hoary',
-    'driver': 'Debian',
-    'url': 'http://security.ubuntu.com/ubuntu',
-    'version': 'hoary-security',
-    'arch': 'i386',
-    'modules': [ 'main' ],
-}]
+config.append (dict (
+    repo= 'fedora-updates',
+    repoUrl= 'http://apt.kde-redhat.org/apt/kde-redhat',
+    repoDir= 'fedora/updates',
+    driver= 'Yum',
+    releases= [ '3' ],
+    archs= [ 'i386' ],
+    baseDirTemplate= '%(release)s/%(arch)s',
+    rpmDir= '.',
+    debug= False,
+    source= False,
+))
 
-config+= [{
-    'local': 'ubuntu-warty',
-    'driver': 'Debian',
-    'url': 'http://archive.ubuntu.com/ubuntu',
-    'version': 'warty',
-    'arch': 'i386',
-    'modules': [ 'main', 'restricted', 'universe' ],
-}]
+# http://apt.kde-redhat.org/apt/kde-redhat/fedora/4/i386/stable/
+config.append (dict (
+    repo= 'kde-redhat',
+    repoUrl= 'http://apt.kde-redhat.org/apt/kde-redhat',
+    repoDir= 'fedora/kde-redhat',
+    driver= 'Yum',
+    distros= [ 'fedora' ],
+    releases= [ '4' ],
+    archs= [ 'i386' ],
+    modules= [ 'testing' ],
+    baseDirTemplate= '%(distro)s/%(release)s/%(arch)s/%(module)s',
+    rpmDir= '.',
+    debug= False,
+    source= False,
+))
 
-config+= [{
-    'local': 'ubuntu/updates/breezy',
-    'driver': 'Debian',
-    'url': 'http://security.ubuntu.com/ubuntu',
-    'version': 'breezy-security',
-    'arch': 'i386',
-    'modules': [ 'main' ],
-}]
-
-config+= [{
-    'local': 'fedora',
-    'driver': 'Yum',
-    'url': 'http://www.las.ic.unicamp.br/pub/fedora/linux/core',
-    'version': '4',
-    'arch': 'i386',
-    'modules': [ '' ],
-    'baseDir': "%(version)s/%(arch)s/os",
-    'rpmDir': 'Fedora/RPMS',
-}]
-
-config+= [{
-    'local': 'fedora/updates',
-    'driver': 'Yum',
-    'url': 'http://www.las.ic.unicamp.br/pub/fedora/linux/core/updates',
-    'version': '3',
-    'arch': 'i386',
-    'modules': [ '' ],
-    'baseDir': "%(version)s/%(arch)s",
-    'rpmDir': '.',
-}]
+config.append (dict (
+    repo= 'debian',
+    repoUrl= 'http://http.us.debian.org/debian',
+    repoDir= 'debian/debian',
+    driver= 'Debian',
+    releases= [ 'sarge' ],
+    archs= [ 'i386' ],
+    modules= [ 'main', 'non-free', 'contrib', 'main/debian-installer' ],
+    baseDirTemplate= '',
+))
