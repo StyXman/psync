7a7f5d40b4e196ae12d4f5e95d2032c7e02c7a96
mdione <>
2005-12-29 04:11:50 +0000

    M    __init__.py * log to stderr. introduces a bug: when broken pipe, *lots* of these appear:
    Traceback (most recent call last):
      File "/usr/lib/python2.3/logging/__init__.py", line 682, in emit
          self.flush()
      File "/usr/lib/python2.3/logging/__init__.py", line 661, in flush
          self.stream.flush()
    IOError: [Errno 32] Broken pipe
    
      will be fixed later.

--- psyncpkg/__init__.py
+++ psyncpkg/__init__.py
@@ -2,10 +2,14 @@
 # Marcos Dione <mdione@grulic.org.ar>
 
 import logging
+import sys
 
-handler = logging.StreamHandler()
+handler = logging.StreamHandler(sys.stdout)
 formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s",
-                                '%H:%M:%S')
+                              '%H:%M:%S')
+# formatter = logging.Formatter(
+#     "%(asctime)s %(levelname)-8s %(filename)8s:%(lineno)-3s %(message)s",
+#     '%H:%M:%S')
 handler.setFormatter(formatter)
 logger = logging.getLogger('psync')
 logger.addHandler(handler)
