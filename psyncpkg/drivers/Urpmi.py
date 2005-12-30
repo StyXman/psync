b57a55d0440bb259b285aa6030e9e771cb082ae8
mdione <>
2005-12-30 21:31:09 +0000

    M    Urpmi.py * a workaround for not downloading several times the same database.

--- psyncpkg/drivers/Urpmi.py
+++ psyncpkg/drivers/Urpmi.py
@@ -16,11 +16,23 @@ logger = logging.getLogger('psync.drivers.Urpmi')
 logger.setLevel(logLevel)
 
 class Urpmi (Rpm):
+    def __init__ (self, **kwargs):
+        super (Urpmi, self).__init__ (**kwargs)
+        # this flag is for downloading the databases just once
+        self.first= None
     
     def databases (self):
-        hdsplit= (dirname (self.hdlist), basename (self.hdlist))
-        synthesis= hdsplit[0]+'/synthesis.'+hdsplit[1]
-        return [ (synthesis, True), (self.hdlist, True) ]
+        logger.debug ("%s, %s" % (self.first, self.release))
+        if self.first==self.release:
+            # databases already dowloaded and updated
+            self.tempDir= '.'
+            ans= []
+        else:
+            self.first= self.release
+            hdsplit= (dirname (self.hdlist), basename (self.hdlist))
+            synthesis= hdsplit[0]+'/synthesis.'+hdsplit[1]
+            ans= [ (synthesis, True), (self.hdlist, True) ]
+        return ans
 
     def files(self):
         hdlist= "%(tempDir)s/%(repoDir)s/%(baseDir)s/%(hdlist)s" % self
@@ -35,12 +47,18 @@ class Urpmi (Rpm):
         headerList= rpm.readHeaderListFromFD (pipe.fileno())
         for header in headerList:
             rpmArch= header[rpm.RPMTAG_ARCH]
+            # logger.debug ("rpm %s arch: %s" % (header[1000000], rpmArch))
             if rpmArch==self.arch:
                 # 1000000-> rpm file name, 1000001-> rpm file size
                 yield (self.rpmDir+'/'+header[1000000], header[1000001])
         
         pipe.close ()
 
-    finalDBs= databases
+    def finalDBs (self):
+        hdsplit= (dirname (self.hdlist), basename (self.hdlist))
+        synthesis= hdsplit[0]+'/synthesis.'+hdsplit[1]
+        return [ (synthesis, True), (self.hdlist, True) ]
 
+    # finalDBs= databases
+    
 # end
