6d078bbf5da4a1950e43a303ae344f1df8e62e4b
mdione <>
2006-01-06 00:43:09 +0000

    M    setup.py * make it easy to import the module and extract the version from the package   metadata.

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='psync',
-      version='0.2.4',
-      description="psync is a distribution mirroring utility with consistency in mind.",
-      long_description="""
+data= dict (
+    name='psync',
+    version='0.2.4',
+    description="psync is a distribution mirroring utility with consistency in mind.",
+    long_description="""
 psync is a mirroring utility. its main goal is to be able to update a  
 distribution mirror but keep it as consistent as possible. one of the main  
 problems updating a mirror is that if the mirroring process is stopped for any  
@@ -13,25 +14,30 @@ reason, the mirror stays inconsistent: either some of the packages in the old
 databases were removed or not all the new packages were downloaded. psync aims  
 to fix this. it also aims to support as many distributions as possible. 
 """,
-      author='Marcos D. Dione',
-      author_email='mdione@grulic.org.ar',
-      url='http://plantalta.homelinux.net/~mdione/projects/psync/',
-      license='GPL',
-      packages=['psyncpkg',
-                'psyncpkg.drivers',
-                ],
-      scripts= ['psync.py'],
-      # data_files= [('examples', ['psync.conf.py'])],
-      classifiers=['Development Status :: 3 - Beta',
-                   'Environment :: Console',
-                   'Intended Audience :: System Administrators',
-                   'License :: OSI Approved :: GNU General Public License (GPL)',
-                   'Natural Language :: Spanish',
-                   'Natural Language :: English',
-                   'Operating System :: POSIX',
-                   'Programming Language :: Python',
-                   'Topic :: Internet',
-		   'Topic :: System :: Archiving :: Mirroring',
-		   'Topic :: Utilities',
-                   ],
-      )
+    author='Marcos D. Dione',
+    author_email='mdione@grulic.org.ar',
+    url='http://plantalta.homelinux.net/~mdione/projects/psync/',
+    license='GPL',
+    packages=[
+        'psyncpkg',
+        'psyncpkg.drivers',
+        ],
+    scripts= ['psync.py'],
+    # data_files= [('examples', ['psync.conf.py'])],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: Spanish',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: System :: Archiving :: Mirroring',
+        'Topic :: Utilities',
+        ],
+    )
+      
+if __name__=='__main__':
+    setup(**data)
